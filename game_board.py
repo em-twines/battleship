199ed4fc38a868306906bbd59c8d950f2b8a35e1
emilytwines <emdelbridge@gmail.com>
2022-11-11 18:02:21 -0500

    added logic for run game, bug in play_turn at input

--- game_board.py
+++ game_board.py
@@ -1,5 +1,7 @@
 from player import Player
 import keyboard
+import numpy as np
+
 
 class Game_board:
 
@@ -7,6 +9,40 @@ class Game_board:
         self.player_1 = Player("Player_1")
         self.player_2 = Player("Player_2")
         name = "Game_Board"
+
+
+
+    def run_game(self):
+        self.display_welcome()
+        board_pl_1 = self.show_board(self.player_1, self.player_2)
+        board_pl_2 = self.show_board(self.player_2, self.player_1)
+        round = 0
+        while (np.sum(board_pl_1) > 0 ) and (np.sum(board_pl_1) > 0):
+            turn_hits_1 = self.play_turn(self.player_1, self.player_2, board_pl_1)
+            board_pl_1 = np.add(board_pl_1, turn_hits_1)
+
+            turn_hits_2 = self.play_turn(self.player_2, self.player_1, board_pl_2)
+            board_pl_2 = np.add(board_pl_2, turn_hits_2)
+
+            print(f'Round: {round}')
+            round += 1
+
+
+
+    def play_turn(self, player_1, player_2, board):
+        guess_x = input(f'{player_1.name}: choose your coordinate horizontally (1-20): ')
+        guess_x = int(guess_x)
+        guess_y = input(f'{player_1.name}: choose your coordinate vertically (1-20): ')     
+        guess_y - int(guess_y)
+        turn_board = np.empty(21, np.int32)
+        if board[guess_x][guess_y] == 1:
+            turn_board[guess_x][guess_y] = 1
+        return turn_board
+        
+
+
+
+
 
     def display_welcome(self):
         
@@ -43,18 +79,7 @@ The Submarine is 3.
 The Battleship is 4.
 And the Aircraft Carrier is 5.
 Sink all four ships to win!''')
-
-
-
-
-
-    def run_game(self):
-        self.display_welcome()
-        # self.player_1.define_board()
-        # self.player_1.create_and_compare_matrices(self.player_1.destroyer, self.player_1.submarine, self.player_1.battleship, self.player_1.aircraft_carrier, self.player_1.board_e)
-        self.show_board(self.player_1, self.player_2)
-        self.show_board(self.player_2, self.player_1)
-        
+      
         
 
 
@@ -64,8 +89,7 @@ Sink all four ships to win!''')
         keyboard.wait("Return")
         
         self.player_1.define_board()
-        self.player_1.create_and_compare_matrices(self.player_1.destroyer, self.player_1.submarine, self.player_1.battleship, self.player_1.aircraft_carrier, self.player_1.board_e)
-
+        board = self.player_1.create_and_compare_matrices(self.player_1.destroyer, self.player_1.submarine, self.player_1.battleship, self.player_1.aircraft_carrier, self.player_1.board_e)
         print("Press 'enter' when you are ready to continue.")
         keyboard.wait("Return")
         print('''
@@ -90,6 +114,8 @@ Sink all four ships to win!''')
 
         
         ''')
+        return board
+
 
 
 
