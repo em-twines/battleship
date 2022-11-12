7073ca264aa4f6d3a1b72d4005b735199b189585
emilytwines <emdelbridge@gmail.com>
2022-11-12 15:20:31 -0500

    additions to game_board.py + print-outs and hits showing

--- game_board.py
+++ game_board.py
@@ -1,6 +1,7 @@
-from player import Player
 import keyboard
 import numpy as np
+
+from player import Player
 
 
 class Game_board:
@@ -13,30 +14,47 @@ class Game_board:
 
 
     def run_game(self):
+
         self.display_welcome()
         board_pl_1 = self.show_board(self.player_1, self.player_2)
         board_pl_2 = self.show_board(self.player_2, self.player_1)
         round = 0
-        while (np.sum(board_pl_1) > 0 ) and (np.sum(board_pl_1) > 0):
-            turn_hits_1 = self.play_turn(self.player_1, self.player_2, board_pl_1)
-            board_pl_1 = np.add(board_pl_1, turn_hits_1)
-
-            turn_hits_2 = self.play_turn(self.player_2, self.player_1, board_pl_2)
-            board_pl_2 = np.add(board_pl_2, turn_hits_2)
-
-            print(f'Round: {round}')
+        turn_hits_1 = self.create_blank_board()
+        turn_hits_2 = self.create_blank_board()
+        # turn_hits_1 = np.zeros((21,21),np.int32)
+        # turn_hits_2 = np.zeros((21,21),np.int32)
+        while (np.sum(turn_hits_1) < 434 ) and (np.sum(turn_hits_2) < 434):
             round += 1
+            print(f'Round: {round}')
+            
+            
+            print(f'''Here is your opponent's board, as you know it, {self.player_1.name}: 
+{turn_hits_1}:''')
+
+            turn_hits_to_add_1 = self.play_turn(self.player_1, self.player_2, board_pl_2)
+            turn_hits_1 = np.add(turn_hits_1, turn_hits_to_add_1)
+            
+            
+            print(f'''Here is your opponent's board, as you know it, {self.player_2.name}: 
+{turn_hits_2}:''')
+            turn_hits_to_add_2 = self.play_turn(self.player_2, self.player_1, board_pl_1)
+            turn_hits_2 = np.add(board_pl_2, turn_hits_2)
+           
 
 
 
     def play_turn(self, player_1, player_2, board):
-        guess_x = input(f'{player_1.name}: choose your coordinate horizontally (1-20): ')
+        guess_x = input(f'{player_1.name}: choose your coordinate horizontally (1-20), then press "enter":')
         guess_x = int(guess_x)
-        guess_y = input(f'{player_1.name}: choose your coordinate vertically (1-20): ')     
-        guess_y - int(guess_y)
-        turn_board = np.empty(21, np.int32)
-        if board[guess_x][guess_y] == 1:
+        guess_y = input(f'{player_1.name}: choose your coordinate vertically (1-20), then press "enter":')     
+        guess_y = int(guess_y)
+        turn_board = np.zeros((21,21),np.int32)
+        if board[guess_y][guess_x] == 1:
             turn_board[guess_x][guess_y] = 1
+            print("It's a hit!")
+        else: 
+            print("It's a miss!")
+        print(f"Your turn, {player_2.name}")
         return turn_board
         
 
@@ -58,17 +76,17 @@ The Submarine is 3.
 The Battleship is 4.
 And the Aircraft Carrier is 5.
 
-(Press "enter" to continue.)''')
+(Press "space" to continue.)''')
 
-        keyboard.wait("Return")
+        keyboard.wait("Space")
 
         print('''
 The first player will make their guess, and if they hit any of the spaces containing an enemy ship, 
 that position will change from "0" to "1." Be the first to sink all 4 ships to win!
 
-(Press "enter" to continue.)
+(Press "space" to continue.)
 ''')
-        keyboard.wait("Return")
+        keyboard.wait("Space")
 
 
 
@@ -85,13 +103,13 @@ Sink all four ships to win!''')
 
     def show_board(self, player_1, player_2):
         print(f'''{player_1.name}, your board is ready. {player_2.name}, please look away. 
-{player_1.name}, press "enter" when you are ready to see your board in secret.''')
-        keyboard.wait("Return")
+{player_1.name}, press "space" when you are ready to see your board in secret.''')
+        keyboard.wait("Space")
         
         self.player_1.define_board()
         board = self.player_1.create_and_compare_matrices(self.player_1.destroyer, self.player_1.submarine, self.player_1.battleship, self.player_1.aircraft_carrier, self.player_1.board_e)
-        print("Press 'enter' when you are ready to continue.")
-        keyboard.wait("Return")
+        print('Press "space" when you are ready to continue.')
+        keyboard.wait("Space")
         print('''
         
         
@@ -133,10 +151,30 @@ Sink all four ships to win!''')
 
 
 
-    def display_current_board(self, player):
-        # print(player.current_board)
-        pass
-
+    def create_blank_board(self):
+        board = np.array([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype = np.int32)
+        return board
 
 
 
